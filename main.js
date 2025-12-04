b88f8867a086a4722d6e002cda68c0e3ef8a3496
Echowispp <kauri.sykari190@gmail.com>
2025-12-04 22:13:01 +0200

    added simple error system and connected webgl2 to modify canvas
    basically webgl2 can change the canvas now, also there is a system that shows errors on the page itself instead of having to open the DevTools

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
+//=============
+// DEFINITIONS
+//=============
+
 const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+const gl = canvas.getContext("webgl2");
+
+const errorDiv = document.getElementById("errorDiv");
+const errorParagraph = document.getElementById("errorParagraph");
+
+//=================
+// CANVAS RESIZING
+//=================
 
 window.addEventListener("load", () => {
 	resizeCanvas();
-	exampleDrawFunc();
+	softRender();
 });
 
 function resizeCanvas() {
@@ -12,8 +23,47 @@ function resizeCanvas() {
 	canvas.height = area.height;
 }
 
-// this is here purely for checking that all the files are connected and that the canvas actually draws when prompted, I will actually change the cnavas context to webgl later
-function exampleDrawFunc() {
-	ctx.fillStyle = "#f00";
-	ctx.fillRect(100, 100, 150, 150);
+//================
+// ERROR HANDLING
+//================
+
+function showError(err) {
+	if (!(typeof err === "string" || err instanceof String)) {
+		err = str(err);
+	}
+
+	if (errorDiv.hidden == true) {
+		errorDiv.hidden = false;
+	}
+
+	errorParagraph.textContent = err;
+	console.log(err);
+}
+
+//===========
+// RENDERING
+//===========
+
+function softRender() {
+	try {
+		renderTriangle();
+	} catch (e) {
+		showError(`Error caught in renderTriangle(), error cause: ${e}`);
+	}
+}
+
+function renderTriangle() {
+	if (!canvas) {
+		showError("Error: canvas not found at renderTriangle()");
+		return;
+	}
+	if (!gl) {
+		showError(
+			"Error: WebGL2 context not found, this liekly means your browser doesn't support it - this demo will not work, you can try another browser"
+		);
+		return;
+	}
+
+	gl.clearColor(1, 0, 0, 1);
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 }
